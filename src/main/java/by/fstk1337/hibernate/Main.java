e6b5c1b526794db842dbe3904266b80f5ad9fa53
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2025-05-28 00:48:48 +0300

    feat: create User entity

--- src/main/java/by/fstk1337/hibernate/Main.java
+++ src/main/java/by/fstk1337/hibernate/Main.java
@@ -1,11 +1,23 @@
 package by.fstk1337.hibernate;
 
+import by.fstk1337.hibernate.entity.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SessionFactory sf = new Configuration().buildSessionFactory();
-        sf.close();
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        try (SessionFactory sf = configuration.buildSessionFactory();
+             Session session = sf.openSession()) {
+            session.beginTransaction();
+            Query<User> query = session.createQuery("from User", User.class);
+            List<User> users = query.list();
+            users.forEach(System.out::println);
+            session.getTransaction().commit();
+        }
     }
 }
---
+++ src/main/java/by/fstk1337/hibernate/entity/User.java
@@ -0,0 +1,99 @@
+package by.fstk1337.hibernate.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "age", nullable = true)
+    private Integer age;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public User() {
+    }
+
+    public User(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        createdAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setActive(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email, age);
+    }
+}
